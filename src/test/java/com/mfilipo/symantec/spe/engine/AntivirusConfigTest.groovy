98ab4df99b89a99d78c0c79732181e1093b2bc4c
filipowm <mateusz.filipowicz@roche.com>
2016-03-03 09:03:02 +0100

    extended AntivirusConfigTest to include positive test

--- src/test/java/com/mfilipo/symantec/spe/engine/AntivirusConfigTest.groovy
+++ src/test/java/com/mfilipo/symantec/spe/engine/AntivirusConfigTest.groovy
@@ -4,7 +4,6 @@ import com.mfilipo.symantec.spe.engine.config.AntivirusConfig
 import spock.lang.Specification
 import spock.lang.Unroll
 
-
 /**
  * Created by filipowm on 2016-03-03.
  */
@@ -30,5 +29,27 @@ class AntivirusConfigTest extends Specification {
         'policy' | null
         'failRetryTime' | -1
         'readWriteTime' | -1
+    }
+
+    void 'config validation passed' () {
+        given:
+        AntivirusConfig config = new AntivirusConfig('test', 1)
+        config.@"$field" = value
+
+        when:
+        config.validate()
+
+        then:
+        noExceptionThrown()
+
+        where:
+        field | value
+        'host' | 'test'
+        'host' | '127.0.0.1'
+        'port' | 1
+        'failRetryTime' | 0
+        'failRetryTime' | 1
+        'readWriteTime' | 0
+        'readWriteTime' | 1
     }
 }
